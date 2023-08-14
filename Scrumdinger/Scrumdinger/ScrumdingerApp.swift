cff6e6dd80da85011d0696c53affd17d7a7e0bdc
Sarah Clark <sarahireneclark@gmail.com>
2023-08-14 17:27:51 -0500

    Initial commit of project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Sarah Clark on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
